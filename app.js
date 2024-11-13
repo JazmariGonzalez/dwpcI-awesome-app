394f76f58c4b4c6c558d98c4ff762d4f46cd8e6c
Jazmari González <158227907+JazmariGonzalez@users.noreply.github.com>
2024-11-13 17:33:49 +0000

    ⭐ FEAT: Routes and middlewares

--- app.js
+++ app.js
@@ -8,8 +8,21 @@ import express from 'express';
 const app = express();
 
 // Registrando el segundo middleware
-app.use((req, res, next)=>{
-  console.log("📢 Middleware #2");
+// Ruta about
+// GET /about
+app.use('/about',(req, res)=>{
+  console.log("📢 Sirviendo la ruta '/about'");
+  // Se contesta al server
+  res.send(`
+    <h1>🪄 About...</h1>
+    <p>App for Fullstack Web Dev Course I!</p>
+  `);
+});
+
+// Ruta Raíz
+// GET /
+app.use((req, res)=>{
+  console.log("📢 Sirviendo la ruta '/'");
   // Se contesta al server
   res.send(`
     <h1>Welcome to Express Js</h1>
@@ -20,6 +33,7 @@ app.use((req, res, next)=>{
 // Definiendo puertos
 const port = 3000;
 const ip = "0.0.0.0"
+
 // Arrancando el servidor
 app.listen(port, ip, () => {
   console.log(`🤖 Sirviendo en http://localhost:${port}`);
