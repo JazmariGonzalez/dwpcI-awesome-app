d4fb28cf8d8efd3e9ec71ac8ace400c2c1e673f3
Jazmari González <158227907+JazmariGonzalez@users.noreply.github.com>
2024-11-16 02:51:15 +0000

    🌟 FEAT: Vistas Estaticas

--- routes /shop.routes.js
+++ routes /shop.routes.js
@@ -1,16 +1,18 @@
 // Importando el enrutador de express
 import { Router } from 'express';
+
+// Importando el gestor de rutas
+import path from 'path';
+
 // Creando una instancia del enrutador de express
 const router = Router();
+
 // GET /
 router.get('/', (req, res)=>{
   console.log("📢 Sirviendo la ruta '/'");
-  // Se contesta al server
-  res.send(`
-    <h1>Welcome to Express Js</h1>
-    <p>This is my awesome app! 😎</p>
-  `);
+  res.sendFile(path.resolve('views','add-product.html'));
 });
+
 // GET /about
 router.get('/about', (req, res) => {
   console.log("📢 Sirviendo la ruta '/about'");
@@ -20,4 +22,6 @@ router.get('/about', (req, res) => {
     <p>App for Fullstack Web Dev Course I!</p>
   `);
 });
+
+// Exportando el enrutador admin
 export default router;
